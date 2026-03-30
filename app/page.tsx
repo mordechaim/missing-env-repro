162c96facdf0d33e26407691e259954ca7a694c0
Mordechai Meisels <mordechai@codaworks.com>
2026-03-30 14:32:07 -0400

    Reproduction

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import { connection } from 'next/server';
+import { Suspense } from 'react';
+
+export default function DotEnvInspector() {
+  return (
+    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+      <StaticInspector />
+      <Suspense fallback={<div>dynamic MY_DOTENV: Loading...</div>}>
+        <DynamicInspector />
+      </Suspense>
+    </div>
+  );
+}
+
+export function StaticInspector() {
+  const dotenv = process.env.MY_DOTENV;
+
+  return <div>static MY_DOTENV: {dotenv == null ? 'undefined' : dotenv}</div>;
+}
+
+export async function DynamicInspector() {
+  await connection();
+
+  const dotenv = process.env.MY_DOTENV;
+
+  return <div>dynamic MY_DOTENV: {dotenv == null ? 'undefined' : dotenv}</div>;
+}
